a61b10a821562f668b29e76a54450a1ff7e51232
AlvaroJorge <alv.jor96@gmail.com>
2022-03-10 20:40:38 -0300

    Javascript terminado

--- index.js
+++ index.js
@@ -10,30 +10,85 @@ class Calculadora{
     constructor(numPrevioIngresado, numActualIngresado){
         this.numPrevioIngresado = numPrevioIngresado;
         this.numActualIngresado = numActualIngresado;
+        this.limpiar();
     }
 
     limpiar(){
-
+        this.operandoActual = '';
+        this.operandoPrevio = '';
+        this.operacion = undefined;
     }
 
     borrar(){
-
-    }
-
-    calcular(){
-
+        this.operandoActual = this.operandoActual.toString().slice(0, -1);
     }
 
     agregarNumero(numero){
-        this.operandoActual = numero;
+        if(numero === '.' && this.operandoActual.includes('.')) return;
+        this.operandoActual = this.operandoActual.toString() + numero.toString();
     }
 
-    elegirOperacion(){
+    elegirOperacion(operacion){
+        if(this.operandoActual === '') return;
+        if(this.operandoPrevio !== ''){
+            this.calcular();
+        }
+        this.operacion = operacion;
+        this.operandoPrevio = this.operandoActual;
+        this.operandoActual = '';
+    }
 
+    calcular(){
+        let computar;
+        const previo = parseFloat(this.operandoPrevio);
+        const actual = parseFloat(this.operandoActual);
+        if(isNaN(previo) || isNaN(actual)) return;
+        switch (this.operacion){
+            case '+':
+                computar = previo + actual;
+                break
+            case '-':
+                computar = previo - actual;
+                break
+            case '*':
+                computar = previo * actual;
+                break
+            case '÷':
+                computar = previo / actual;
+                break
+            default:
+                return;        
+        }
+        this.operandoActual = computar;
+        this.operacion = undefined;
+        this.operandoPrevio = '';
+        
+    }
+
+    mostrarComasNumero(numero){
+        const numeroString = numero.toString();
+        const digitoEntero = parseFloat(numeroString.split('.')[0]);
+        const digitoDecimal = numeroString.split('.')[1];
+        let digitoMostrado;
+        if(isNaN(digitoEntero)){
+            digitoMostrado = '';
+        } else {
+            digitoMostrado = digitoEntero.toLocaleString('en', {maximumFractionDigits: 0});
+        }
+        if(digitoDecimal != null){
+            return `${digitoMostrado}.${digitoDecimal}`;
+        } else {
+            return digitoMostrado;
+        }
     }
 
     actualizarNumeros(){
-        this.numActualIngresado.innerText = this.agregarNumero(this.operandoActual);
+        this.numActualIngresado.innerText = this.mostrarComasNumero(this.operandoActual);
+        if(this.operacion != null){
+            this.numPrevioIngresado.innerText = `${this.mostrarComasNumero(this.operandoPrevio)} ${this.operacion}`;
+        } else {
+            this.numPrevioIngresado.innerText = '';
+        }
     }
 }
 
@@ -45,3 +100,26 @@ numerosBtn.forEach(button => {
         calculadora.actualizarNumeros();
     })
 })
+
+operadorBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        calculadora.elegirOperacion(button.innerText);
+        calculadora.actualizarNumeros();
+    })
+})
+
+igualBtn.addEventListener('click', () => {
+    calculadora.calcular();
+    calculadora.actualizarNumeros();
+})
+
+limpiarBtn.addEventListener('click', () => {
+    calculadora.limpiar();
+    calculadora.actualizarNumeros();
+})
+
+borrarBtn.addEventListener('click', () => {
+    calculadora.borrar();
+    calculadora.actualizarNumeros();
+})
+
